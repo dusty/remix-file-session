041befb3861ce8e91077e7646ff77e4b0b2c3fe7
Dusty Doris <dusty@postal.io>
2022-08-06 14:26:04 -0700

    log

--- app/routes/__thing/index.tsx
+++ app/routes/__thing/index.tsx
@@ -1,13 +1,13 @@
 import type { ActionArgs, LoaderArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { Form } from "@remix-run/react";
+import { Form, useLoaderData } from "@remix-run/react";
 import { commitSession, getUserSession } from "~/session.server";
 
 export async function loader(args: LoaderArgs) {
   const session = await getUserSession(args.request);
   session.set("index", true);
   return json(
-    { index: true },
+    { index: true, session },
     { headers: { "set-cookie": await commitSession(session) } }
   );
 }
@@ -21,6 +21,8 @@ export async function action(args: ActionArgs) {
 }
 
 export default function Index() {
+  const { session } = useLoaderData();
+  console.log("SESSION", session);
   return (
     <Form method="post">
       <button name="thing" value="stuff">
